b22ba58485ca2b372886cb3a39caf1ad9da5ecaf
valdirapn <valdiraires.pn@gmail.com>
2023-02-14 08:26:57 -0300

    first commit

---
+++ src/test/java/br/edu/ufersa/blockbuster/BlockbusterApplicationTests.java
@@ -0,0 +1,13 @@
+package br.edu.ufersa.blockbuster;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlockbusterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
