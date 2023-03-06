a8d060f8facf2d301e2c9d3504ce19d4a0599ea9
Lucas Vinicius Fernandes da Silva <lucassilved@gmail.com>
2023-03-05 21:44:47 -0300

    feat(SerieController): create methods: list and search by uuid and title

--- src/main/java/br/edu/ufersa/blockbuster/api/restControllers/SerieController.java
+++ src/main/java/br/edu/ufersa/blockbuster/api/restControllers/SerieController.java
@@ -1,11 +1,17 @@
 package br.edu.ufersa.blockbuster.api.restControllers;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
+
 import javax.validation.Valid;
 
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -36,5 +42,38 @@ public class SerieController {
 
     SerieDto created = mapper.map(serie, SerieDto.class);
     return new ResponseEntity<>(created, HttpStatus.CREATED);
+  }
+
+  @GetMapping
+  public List<SerieDto> list() {
+    List<SerieDto> seriesDto = new ArrayList<SerieDto>();
+
+    for (Serie serie : this.serieService.getAll()) {
+      seriesDto.add(this.mapper.map(serie, SerieDto.class));
+    }
+
+    return seriesDto;
+  }
+
+  @GetMapping("/{id}")
+  public ResponseEntity<SerieDto> search(@PathVariable UUID id) {
+    SerieDto dto = this.mapper.map(this.serieService.getByUuid(id), SerieDto.class);
+
+    if (dto != null) {
+      return new ResponseEntity<>(dto, HttpStatus.OK);
+    }
+
+    return ResponseEntity.notFound().build();
+  }
+
+  @GetMapping("/{title}")
+  public ResponseEntity<SerieDto> search(@PathVariable String title) {
+    SerieDto dto = this.mapper.map(this.serieService.getByTitle(title), SerieDto.class);
+
+    if (dto != null) {
+      return new ResponseEntity<>(dto, HttpStatus.OK);
+    }
+
+    return ResponseEntity.notFound().build();
   }
 }
